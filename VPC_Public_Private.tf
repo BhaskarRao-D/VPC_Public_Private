be9299e3619d86df0003928bc5d78c709ad9b9c4
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-06-21 20:01:35 +0530

    Create VPC_Public_Private.tf

---
+++ VPC_Public_Private.tf
@@ -0,0 +1,60 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.publicsubnet.id
+  depends_on    = [aws_internet_gateway.ig]
+}
+
+resource "aws_subnet" "publicsubnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "privatesubnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+}
+
+resource "aws_route_table" "rt_private" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "PublicRTassociation" {
+  subnet_id      = aws_subnet.publicsubnet.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "PrivateRTassociation" {
+  subnet_id      = aws_subnet.privatesubnet.id
+  route_table_id = aws_route_table.rt_private.id
+}
